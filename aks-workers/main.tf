b86597729d28778bc9500ea499ca219cf820cc1c
Joseph Masengesho <jmasengesho@inyarwanda.com>
2023-06-21 13:15:25 -0500

    added pod subnet

--- aks-workers/main.tf
+++ aks-workers/main.tf
@@ -1,5 +1,13 @@
+
+data "azurerm_subnet" "pod_subnet" {
+  name                 = var.pod_subnet_name
+  virtual_network_name = var.spoke_vnet_name
+  resource_group_name  = var.spoke_resource_group_name
+}
+
 module "worker_pool" {
   source                      = "../modules/aks-node-pools/"
   aks_cluster_resource_group  = var.aks_cluster_resource_group
   aks_cluster_name            = var.aks_cluster_name
+  pod_subnet_id               = data.azurerm_subnet.pod_subnet.id
 } 
